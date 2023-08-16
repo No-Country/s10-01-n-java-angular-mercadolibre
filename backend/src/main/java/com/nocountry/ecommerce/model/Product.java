2135cddc6a4dd30ec312f40c1be4ae32a9a86ad5
Diego Villan <dnvx81@outlook.com>
2023-08-15 22:53:40 -0300

    build(backend): cloning  current project

--- backend/src/main/java/com/nocountry/ecommerce/model/Product.java
+++ backend/src/main/java/com/nocountry/ecommerce/model/Product.java
@@ -1,4 +1,27 @@
 package com.nocountry.ecommerce.model;
 
+import com.nocountry.ecommerce.util.Category;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Product {
+    @Id
+    private int id;
+    @NotBlank
+    private String name;
+    private String description;
+    @NotNull
+    private Double price;
+    private Category category;
 }
