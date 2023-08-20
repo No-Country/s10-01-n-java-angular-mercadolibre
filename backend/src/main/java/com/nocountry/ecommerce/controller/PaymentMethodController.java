52f24c3864fdb41f799d116beec913c0bf8b2653
Wilder <wilderruizrpg@gmail.com>
2023-08-20 06:45:44 -0500

    feat(PMController): added validations to endpoints create & delete

--- backend/src/main/java/com/nocountry/ecommerce/controller/PaymentMethodController.java
+++ backend/src/main/java/com/nocountry/ecommerce/controller/PaymentMethodController.java
@@ -4,6 +4,7 @@ import com.nocountry.ecommerce.model.PaymentMethod;
 import com.nocountry.ecommerce.service.PaymentMethodIMPL;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -106,6 +107,65 @@ public class PaymentMethodController {
     @RequestMapping(value = "modifyPaymentMethod", method = RequestMethod.PUT)
     public ResponseEntity<?> modifyPaymentMethod(@RequestBody PaymentMethod paymentMethod) {
 
+        if(paymentMethod.getCardType() == null || 
+           paymentMethod.getCardType().isEmpty() ||
+           paymentMethod.getCardType().isBlank())
+           return new ResponseEntity<>("Card type can't be empty", HttpStatus.BAD_REQUEST );
+        
+        if(!paymentMethod.getCardType().matches("^[a-zA-Z]+$"))
+            return new ResponseEntity<>("Card type must contain only letters", HttpStatus.NOT_ACCEPTABLE);
+        
+        if(paymentMethod.getCardNumber() == null || 
+           paymentMethod.getCardNumber().isEmpty() || 
+           paymentMethod.getCardNumber().isBlank())
+           return new ResponseEntity<>("Card number can't be empty", HttpStatus.BAD_REQUEST);
+        
+        if((paymentMethod.getCardNumber().length()<15 || 
+            paymentMethod.getCardNumber().length()>16))
+            return new ResponseEntity<>("Please enter a valid number card", HttpStatus.NOT_ACCEPTABLE);
+        
+        if(!paymentMethod.getCardNumber().matches("\\d+"))
+            return new ResponseEntity<>("Card Number must contain only digits", HttpStatus.BAD_REQUEST);
+        
+        if(paymentMethod.getSecurityCode() == null ||
+           paymentMethod.getSecurityCode().isEmpty() ||
+           paymentMethod.getSecurityCode().isBlank())
+           return new ResponseEntity<>("Security code can't be empty", HttpStatus.BAD_REQUEST);
+        
+        if(!paymentMethod.getSecurityCode().matches("\\d+"))
+            return new ResponseEntity<>("Security code must contain only digits", HttpStatus.NOT_ACCEPTABLE);
+        
+        if((paymentMethod.getSecurityCode().length()<3 || 
+           paymentMethod.getSecurityCode().length()>4))
+           return new ResponseEntity<>("Please enter a valid security code", HttpStatus.BAD_REQUEST);
+        
+        if(paymentMethod.getExpirationDate() == null || 
+           paymentMethod.getExpirationDate().isEmpty() ||
+           paymentMethod.getExpirationDate().isBlank())
+           return new ResponseEntity<>("Expiration date can't be empty", HttpStatus.BAD_REQUEST);
+        
+        if(!paymentMethod.getExpirationDate().matches("^(0[1-9]|1[0-2])/(\\d{2}|\\d{4})$")) 
+            return new ResponseEntity<>("Invalid expiration date format", HttpStatus.BAD_REQUEST);
+        
+        char x = paymentMethod.getExpirationDate().charAt(0);
+        char z = paymentMethod.getExpirationDate().charAt(1);
+        
+        if (x < 0 && z > 9) return new ResponseEntity<>("Please enter a valid month", HttpStatus.NOT_ACCEPTABLE);
+
+        if (x > 9 && z < 0) return new ResponseEntity<>("Please enter a valid month", HttpStatus.NOT_ACCEPTABLE);
+        
+        String [] parts = paymentMethod.getExpirationDate().split("/");
+        
+        int year = Integer.parseInt(parts[1]);
+        int currentYear = LocalDate.now().getYear();
+        
+        if (year > currentYear + 7 || year < currentYear) return new ResponseEntity<>("Please enter a valid expiration date", HttpStatus.NOT_ACCEPTABLE);
+        
+        if(paymentMethod.getBillingAddress() == null ||
+           paymentMethod.getBillingAddress().isEmpty() ||
+           paymentMethod.getBillingAddress().isBlank())
+           return new ResponseEntity<>("Billing address can't be empty", HttpStatus.BAD_REQUEST);
+        
         PaymentMethod Payment = this.pmimpl.modifyPaymentMethod(paymentMethod);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(Payment);
@@ -125,9 +185,16 @@ public class PaymentMethodController {
     @RequestMapping(value = "deletePaymentMethod/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<?> deletePaymentMethod(@PathVariable int id) {
 
-        this.pmimpl.deletePaymentMethod(id);
-
-        return ResponseEntity.ok().build();
-
+        PaymentMethod payment = null;
+        
+        try {
+        payment = this.pmimpl.consultPaymentMethod(id);
+        if(payment != null) this.pmimpl.deletePaymentMethod(id);
+        
+        } catch(NoSuchElementException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("The given payment method doesn't exits", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(payment, HttpStatus.OK);
     }
 }
