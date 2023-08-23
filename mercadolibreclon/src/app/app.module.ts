a92364753576e3cfb9d873a92eaa263e983e9480
Afalkenhagen <ahau_buluk@protonmail.ch>
2023-08-23 20:54:15 -0300

    feat: login

--- mercadolibreclon/src/app/app.module.ts
+++ mercadolibreclon/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AuthLoginComponent } from './components/auth-login/auth-login.component';
+import { AuthLoginBisComponent } from './components/auth-login-bis/auth-login-bis.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthLoginComponent,
+    AuthLoginBisComponent,
   ],
   imports: [
     BrowserModule,
