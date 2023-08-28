c55f540c668aecd5646632e4b9e20533b913eb8f
Elcira Ibarra <elcira.rpi@gmail.com>
2023-08-28 01:08:13 -0300

    Newly created components

--- mercadolibreclon/src/app/app.module.ts
+++ mercadolibreclon/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HomeComponent } from './pages/home/home.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SliderComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
