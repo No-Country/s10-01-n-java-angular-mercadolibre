58c91fbfe38969b3351146f48b601ce48c538b62
Wilder <wilderruizrpg@gmail.com>
2023-08-28 22:14:50 -0500

    feat(Account & PM): created relationship

--- backend/src/main/java/com/nocountry/ecommerce/model/Account.java
+++ backend/src/main/java/com/nocountry/ecommerce/model/Account.java
@@ -12,6 +12,7 @@ import lombok.ToString;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -51,6 +52,9 @@ public class Account implements Serializable {
     private String tokenPassword;
     @Transient
     private String token;
+    
+    @OneToMany(mappedBy = "account")
+    private List<PaymentMethod> payments;
     
 
     public Account(String email, String password){
