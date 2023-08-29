57e0d90f4b5295c69626d355cc274317e5922fbf
Wilder <wilderruizrpg@gmail.com>
2023-08-28 19:39:35 -0500

    feat(PM & SA): Implemented serialVersion

--- backend/src/main/java/com/nocountry/ecommerce/model/PaymentMethod.java
+++ backend/src/main/java/com/nocountry/ecommerce/model/PaymentMethod.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,7 +20,8 @@ import lombok.ToString;
 @Setter
 @Entity
 @Table(name = "payment_method")
-public class PaymentMethod {
+public class PaymentMethod implements Serializable {
+    private static final long serialVersionUID = 1L;
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
