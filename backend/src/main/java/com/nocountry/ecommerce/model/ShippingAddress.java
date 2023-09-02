ce9251d61ad3144c8f92cffc01483f67f8a47de0
Wilder <wilderruizrpg@gmail.com>
2023-09-02 18:01:21 -0500

    feat(Account & SA): created relationship

--- backend/src/main/java/com/nocountry/ecommerce/model/ShippingAddress.java
+++ backend/src/main/java/com/nocountry/ecommerce/model/ShippingAddress.java
@@ -2,9 +2,12 @@ package com.nocountry.ecommerce.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.io.Serializable;
 import lombok.AllArgsConstructor;
@@ -37,4 +40,8 @@ private static final long serialVersionUID = 1L;
     private String country;
     @Column(name = "phone")
     private String phone;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_account", referencedColumnName = "account_uuid")
+    private Account account;
 }
