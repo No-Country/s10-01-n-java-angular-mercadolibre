f4917b428e67fe2fe1935f7a7a5b59705d1feab8
Elcira Ibarra <elcira.rpi@gmail.com>
2023-09-13 04:26:11 -0300

    Component Checkout Standard created

---
+++ mercadolibreclon/src/app/pages/cart/components/checkout-standard/checkout-standard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { faArrowLeft  } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-checkout-standard',
+  templateUrl: './checkout-standard.component.html',
+  styleUrls: ['./checkout-standard.component.scss']
+})
+export class CheckoutStandardComponent {
+
+  faArrowLeft = faArrowLeft;
+
+}
