3c06b2a35d05dc5e5161dd8d2b2ddd89ef25690c
Elcira Ibarra <elcira.rpi@gmail.com>
2023-09-13 04:24:30 -0300

    Component Delivery Method created

---
+++ mercadolibreclon/src/app/pages/cart/components/delivery-method/delivery-method.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { faArrowLeft  } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-delivery-method',
+  templateUrl: './delivery-method.component.html',
+  styleUrls: ['./delivery-method.component.scss']
+})
+export class DeliveryMethodComponent {
+
+  faArrowLeft = faArrowLeft;
+
+}
