97c96a371d0fdee3a86c5480384e64cfed10103c
Elcira Ibarra <elcira.rpi@gmail.com>
2023-09-13 04:26:39 -0300

    Component Checkout MercadoPago created

---
+++ mercadolibreclon/src/app/pages/cart/components/checkout-mercadopago/checkout-mercadopago.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { faArrowLeft  } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-checkout-mercadopago',
+  templateUrl: './checkout-mercadopago.component.html',
+  styleUrls: ['./checkout-mercadopago.component.scss']
+})
+export class CheckoutMercadopagoComponent {
+
+  faArrowLeft = faArrowLeft;
+
+}
