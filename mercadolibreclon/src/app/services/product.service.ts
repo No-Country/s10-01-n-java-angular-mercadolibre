9ae51fa50d5b4aeabb587c5dce78b5c3a0363e78
Elcira Ibarra <elcira.rpi@gmail.com>
2023-09-13 04:42:28 -0300

    Product Service updated

--- mercadolibreclon/src/app/services/product.service.ts
+++ mercadolibreclon/src/app/services/product.service.ts
@@ -14,7 +14,7 @@ export class ProductService {
       title: 'Moto E22 gb 2 gb Ram Gris 1',
       price: 49000,
       cuotas: 49000 / 6,
-      shipping: 'Envío gratis'
+      shipping: 'Gratis'
     },
     {
       id: '2',
@@ -22,7 +22,7 @@ export class ProductService {
       title: 'Moto E22 gb 2 gb Ram Gris 2',
       price: 69000,
       cuotas: 69000 / 6,
-      shipping: 'Envío gratis'
+      shipping: 'Gratis'
     },
     {
       id: '3',
@@ -30,7 +30,7 @@ export class ProductService {
       title: 'Moto E22 gb 2 gb Ram Gris 3',
       price: 89000,
       cuotas: 89000 / 6,
-      shipping: 'Envío gratis'
+      shipping: 'Gratis'
     },
   ]
 
@@ -50,4 +50,11 @@ export class ProductService {
     return this.cart.length;
   }
 
+  removeFromCart(productId: string) {
+    const index = this.cart.findIndex(product => product.id === productId);
+    if (index !== -1) {
+      this.cart.splice(index, 1);
+
+    }
+  }
 }
