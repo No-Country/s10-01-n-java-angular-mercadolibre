75fe64da94116429bd66ac272c76718be170d424
Elcira Ibarra <elcira.rpi@gmail.com>
2023-09-15 00:45:47 -0300

    Updating the logic to fetch product details

--- mercadolibreclon/src/app/components/product-detail/product-detail.component.ts
+++ mercadolibreclon/src/app/components/product-detail/product-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from 'src/app/services/product.service';
+import { faStar, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-product-detail',
@@ -8,6 +10,9 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit{
+
+  faStar = faStar;
+  faArrowRight = faArrowRight;
 
   product: any;
   cart: any[] = [];
@@ -17,14 +22,28 @@ export class ProductDetailComponent implements OnInit{
     private productService: ProductService
   ) {}
 
+  productId: any;
+  productDetails: any;
+
+
   ngOnInit() {
     window.scrollTo({ top: 0 });
     const id = this.route.snapshot.paramMap.get('id');
     console.log(id)
-    const products = this.productService.getProducts();
-    this.product = products.find(p => p.id === id);
+    // const products = this.productService.getProducts();
+    const products = this.productService.getProductsFake().subscribe((data) => {
+      this.product = data.find((p: { id: string | null; }) => p.id === id);
+    });
 
     this.getCart();
+
+
+    this.route.params.subscribe(params => {
+      this.productId = +params['id'];
+      this.productService.getProductDetails(this.productId).subscribe((data) => {
+        this.productDetails = data;
+      });
+    });
   }
 
   addCart(product: any) {
